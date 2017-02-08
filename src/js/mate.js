e1211418354c4d05e4a41bc3d3de45e5a7be5aac
Jérémy Berthelier <jeremyberthelier@hotmail.fr>
2017-02-08 14:20:10 +0000

    Tabulation changes (2 spaces)

--- src/js/mate.js
+++ src/js/mate.js
@@ -1,30 +1,30 @@
 'use strict';
 
 const Mate = {
-    modal: (el) => {
-        const action = (val) => {
-            const   modal = document.querySelector(el),
-                    getClass =  modal.getAttribute('class'),
-                    openClass = 'open';
-            let     newClass = '';
-            
-            if (val == 'open') {
-                newClass = (getClass.search(openClass) == -1 ? getClass + ' ' + openClass : getClass);
-            } else {
-                newClass = getClass.replace(openClass, '');
-            }
-            modal.setAttribute('class', newClass);
-        };
-        
-        return {
-            open: () => {
-                action('open');
-            },
-            close: () => {
-                action('close');
-            }
-        }
+  modal: (el) => {
+    const action = (val) => {
+      const modal = document.querySelector(el),
+            getClass =  modal.getAttribute('class'),
+            openClass = 'open';
+      let   newClass = '';
+      
+      if (val == 'open') {
+        newClass = (getClass.search(openClass) == -1 ? getClass + ' ' + openClass : getClass);
+      } else {
+        newClass = getClass.replace(openClass, '');
+      }
+      modal.setAttribute('class', newClass);
+    };
+    
+    return {
+      open: () => {
+        action('open');
+      },
+      close: () => {
+        action('close');
+      }
     }
+  }
 }
 
 module.exports = Mate;
