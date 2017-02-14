d35ac051ee7ec2532467df3e005818fc844fd924
Jérémy Berthelier <jeremyberthelier@hotmail.fr>
2017-02-14 15:53:26 +0000

    Add to Mate Modal an init phase options

--- src/js/mate.js
+++ src/js/mate.js
@@ -1,7 +1,47 @@
 'use strict';
 
 const Mate = {
-  modal: (id) => {
+  modal: (id, options) => {
+    let   init = false;
+    
+    const defineOptions = {
+            overlay: true,
+            showCloseBtn: true,
+            triggers: {
+              open: '#addItem',
+              close: '.modal-close'
+            }
+          },
+          
+          checkTriggersOptions = (triggers, defaultOptions) => {
+            for (let act in triggers) {
+              if (triggers[act] == undefined) {
+                triggers[act] = defaultOptions[act];
+              }
+            }
+            return triggers;
+          },
+          
+          initOptions = (op) => {
+            if (!op) {
+              op = defineOptions;
+            } else {
+              for (let prop in defineOptions) {
+                if (op[prop] == undefined) {
+                  op[prop] = defineOptions[prop];
+                } else if (prop == 'triggers') {
+                  op[prop] = checkTriggersOptions(op[prop], defineOptions[prop]);
+                }
+              }
+            }
+            init = true;
+            return op;
+          };
+          
+    if (!init) {
+      options = initOptions(options);
+    }
+    
     const action = (val) => {
       const modal = document.querySelector(id),
             getClass =  modal.getAttribute('class'),
@@ -18,18 +58,13 @@ const Mate = {
     
     return {
       register: (openCallback, closeCallBack) => {
-        const modal = Mate.modal(id),
-              trigger = {
-                open: '#addItem',
-                close: '.modal-close'
-              },
-              suffixModule = 'Callback';
+        const modal = Mate.modal(id);
         
-        for (let prop in trigger) {
-          if (trigger[prop]) {
-            let query = document.querySelectorAll(trigger[prop]);
+        for (let prop in options['triggers']) {
+          if (options['triggers'][prop]) {
+            let query = document.querySelectorAll(options['triggers'][prop]);
             if (query.length == 1) {
-              (prop == 'open'? openCallback(trigger[prop], modal) : closeCallBack(trigger[prop], modal));
+              (prop == 'open'? openCallback(options['triggers'][prop], modal) : closeCallBack(options['triggers'][prop], modal));
             } else {
               for (let i = 0; i < query.length; i++) {
                 let getIdFromClass = '#' + query[i].getAttribute('id'); //Important to declare ID from action Trigger button
